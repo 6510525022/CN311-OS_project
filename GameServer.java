313ca526917f2f8ece1ed2880b7cdcb586f40578
Kasidit Kornsang <6510685016@student.tu.ac.th>
2025-05-12 15:13:29 +0700

    add FishState Thread

--- GameServer.java
+++ GameServer.java
@@ -1,9 +1,14 @@
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.concurrent.*;
 
 public class GameServer {
+
+    // setup variable
+    final static int PORT = 1234;
+    static String currentFishState = "";
 
     // hashmap เก็บปลาของ client (รองรับการทำงานหลาย thread)
     public static Map<Socket, Fish> fishMap = new ConcurrentHashMap<>();
@@ -14,12 +19,20 @@ public class GameServer {
     public static List<ClientHandler> handlers = Collections.synchronizedList(new ArrayList<>());
 
     public static void main(String[] args) {
-        final int port = 1234;
+        // สร้าง GameState เพื่อเริ่มต้นแชร์สถานะเกมให้กับ client
         GameState gameState = new GameState();
 
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Server รันอยู่ที่พอร์ต " + port);
+        // สร้าง ServerSocket
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server รันอยู่ที่พอร์ต " + PORT);
 
+            // Thread สำหรับส่ง FishState ให้ client ทุกคนทุก ๆ 33ms
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+            scheduler.scheduleAtFixedRate(() -> {
+                broadcastFishState();
+            }, 0, 33, TimeUnit.MILLISECONDS); //30 per second
+            
+            //รอ client เชื่อมต่อที่ ServerSocket
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("มี Client ใหม่เชื่อมต่อ");
@@ -28,7 +41,7 @@ public class GameServer {
                 Fish playerFish = new Fish(random(100, 500), random(100, 500), 200, "right", "player", true);
                 fishMap.put(clientSocket, playerFish);
 
-                // สร้าง Thread สำหรับ client
+                // สร้าง ClientHandler (Thread) สำหรับ client
                 ClientHandler clientHandler = new ClientHandler(clientSocket, gameState);
                 handlers.add(clientHandler);
                 clientHandler.start();
@@ -40,6 +53,7 @@ public class GameServer {
 
     // ส่งสถานะปลาให้ client ทุกคน
     public static void broadcastFishState() {
+        
         StringBuilder sb = new StringBuilder();
         for (Map.Entry<Socket, Fish> entry : fishMap.entrySet()) {
             Socket socket = entry.getKey();
@@ -54,9 +68,13 @@ public class GameServer {
         }
 
         String data = "สถานะของปลา:" + sb.toString();
+        //ถ้า State ของปลาไม่เปลี่ยนแปลง ก็ไม่ต้องส่งข้อมูลไปให้ client
+        if (currentFishState.equals(data)) return;
+        currentFishState = data;
         for (ClientHandler h : handlers) {
-            h.send(data);
+            h.send(currentFishState);
         }
+        System.err.println(currentFishState);
     }
 
     // method สุ่ม สำหรับใช้สุ่มจุดเกิด
@@ -85,58 +103,59 @@ class ClientHandler extends Thread {
 
     // ใน ClientHandler
     public void run() {
-        try (
-                InputStream input = socket.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                OutputStream output = socket.getOutputStream();) {
-
+        try (InputStream input = socket.getInputStream(); BufferedReader reader = new BufferedReader(new InputStreamReader(input)); OutputStream output = socket.getOutputStream();) 
+        {
             writer = new PrintWriter(output, true);
             writer.println("เชื่อมต่อกับ Server สำเร็จ");
 
             String text;
             while ((text = reader.readLine()) != null) {
-                System.out.println("---------------------( " + text + " )---------------------");
                 int currentPhase = gameState.getGamePhase();
-
                 switch (currentPhase) {
                     case 0: // Lobby phase
-                        writer.println("Lobby Phase");
+                        // ถ้า client ส่งคำสั่งมาให้ server
                         if (text.equals("start")) {
                             gameState.setGamePhase(1); // Change phase เป็น in-game
                             writer.println("เริ่มเกมแล้ว");
+                            writer.println("In-game phase {up, down, left, right, next}");
+                        }else{
+                            writer.println("Lobby phase {start}");
                         }
                         break;
+
                     case 1: // In-game phase
+                        // ถ้า client ส่งคำสั่งมาให้ server
                         Fish playerFish = GameServer.fishMap.get(socket);
                         if (playerFish != null && playerFish.isAlive) {
-
                             if (List.of("up", "down", "left", "right").contains(text)) {
                                 playerFish.move(text);
                             }
                         }
 
-                        // ไม่ต้องรอทุก client ก่อนส่งสถานะ
-                        GameServer.broadcastFishState();
-
-                        // จบเกม
-                        if (text.equals("next")) {
+                        if (text.equals("next")) { // จบเกม
                             gameState.setGamePhase(2); // เปลี่ยน phase แล้วทุก client เห็น
                             writer.println("เกมจบแล้ว");
+                            writer.println("result phase {next}");
+                        } else {
+                            writer.println("In-game phase {up, down, left, right, next}");
                         }
                         break;
 
                     case 2: // Result phase
-                        writer.println("Result Phase");
                         if (text.equals("next")) {
                             gameState.setGamePhase(0); // Return to Lobby phase
                             writer.println("กลับไป Lobby แล้ว");
+                            writer.println("Lobby phase {start}");
+                        } else {
+                            writer.println("result phase {next}");
                         }
                         break;
                 }
-
+            
                 writer.println("END");
-            }
-
+            } 
+        
+            // ปิดการเชื่อมต่อ
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
